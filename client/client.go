ee999f54d5d18177921cebb0ae62828d6beb8b18
Ahsan <ahsanulkh996@gmail.com>
2020-09-15 18:14:45 +0700

    change to read body json

--- client/client.go
+++ client/client.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
-	"strconv"
+	"time"
 
-	"github.com/ahsanulks/testingrpc/proto"
+	"github.com/ahsanulks/testingrpc/client/proto"
 	"github.com/gorilla/mux"
 	"google.golang.org/grpc"
 )
+
+var clientService proto.AddServiceClient
 
 func main() {
 	conn, err := grpc.Dial(":4040", grpc.WithInsecure())
@@ -15,20 +20,51 @@ func main() {
 		panic(err)
 	}
 
-	client := proto.NewAddServiceClient(conn)
+	clientService = proto.NewAddServiceClient(conn)
 
 	r := mux.NewRouter()
-	// r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/add/{x}/{y}", AddHandler)
-	// r.HandleFunc("/articles", ArticlesHandler)
+	r.HandleFunc("/add", AddHandler)
+	r.HandleFunc("/multiple", MultipleHandler).Methods("POST")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
 
 func AddHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	x, _ := strconv.ParseInt(vars["x"], 10, 64)
-	y, _ := strconv.ParseInt(vars["y"], 10, 64)
+	body := make(map[string]int64)
+
+	json.NewDecoder(r.Body).Decode(&body)
+	x, y := body["x"], body["y"]
 
 	req := proto.Request{X: x, Y: y}
+	response, err := clientService.Add(context.Background(), &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	// fmt.Fprintf(w, "Category: %v\n", vars["category"])
+	json.NewEncoder(w).Encode(map[string]int64{"total": response.Result})
+}
+
+func MultipleHandler(w http.ResponseWriter, r *http.Request) {
+	body := make(map[string]int64)
+
+	json.NewDecoder(r.Body).Decode(&body)
+	x, y := body["x"], body["y"]
+
+	req := proto.Request{X: x, Y: y}
+	response, err := clientService.Multiply(context.Background(), &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]int64{"total": response.Result})
 }
---
+++ client/go.mod
@@ -0,0 +1,10 @@
+module github.com/ahsanulks/testingrpc/client
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/gorilla/mux v1.8.0 // indirect
+	google.golang.org/grpc v1.32.0
+	google.golang.org/protobuf v1.25.0
+)
